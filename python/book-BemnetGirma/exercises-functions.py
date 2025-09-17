6d71332759edb89c60677c21b699aeb6f2e7dd02
Siddardh Garlapati <siddardhr1@gmail.com>
2025-09-17 18:46:02 -0500

    Added: exercises-functions.py to book-BemnetGirma

---
+++ python/book-BemnetGirma/exercises-functions.py
@@ -0,0 +1,37 @@
+#create a function to calculate avg of two numbers.
+
+#create a function to calculate a power b using eval function.
+
+#create a function to calculate (return) sum and avg of two numbers.
+
+#create a parametric function to check a number is odd or even.
+
+#create a function to print student id and student name using keyword arguments 
+
+#create a function to print phone brand and phone price using positional keyword arguments 
+
+#create a function to print football player name and number of goals he scored using default arguments. default number = 0.
+
+#create a function to print player name and other details of the player using variable length arguments.
+
+#create a function to calculate average of two numbers and assign return value to a variable.
+
+#create a function to calculate tan(45) using two other functions sine and cosine return values by using eval() function.
+
+#create a function to calculate tan(45)by passing sine and cosine functions as paramenter eval() functions.
+
+#create a function to calculate sum of all numbers from 1 to 100.
+
+#create a lambda function to calculate average of two numbers.
+
+#create a filter function to check a number is odd or even.
+
+#create a list variable and store price of foods then print all food prices after 25% discount using map function.
+
+#create a list variable and store your mid semester exam results of each subject and calculate avg using the reduce function.
+
+#create a decorator to divide two numbers but if a number is divided by zero, print invalid.
+
+#create a decorator to divide two numbers but if numerator is always greater than the denominator.
+
+#create a generator to print top 3 atheles who finish olympic marathon 2019.
