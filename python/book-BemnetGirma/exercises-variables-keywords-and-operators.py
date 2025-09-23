640ca2e4434492b2eaa4b2045e141161a780175e
Siddardh Garlapati <siddardhr1@gmail.com>
2025-09-23 06:05:10 -0500

    Create: exercises-variables-keywords-and-operators.py

---
+++ python/book-BemnetGirma/exercises-variables-keywords-and-operators.py
@@ -0,0 +1,39 @@
+# Create a variable date and store todays date then print it
+
+# Create a variable bd and take input birth date from user then print it
+
+# Create a variable ap and accept apple price from user, then print user’s apple price ap and its memory location, then next reassign apple price to 34, again print apple price and its memory location, finally delete variable ap
+
+# Create a variable gf and print any general fact and its data type?
+
+# Create a variable s and store number of subjects you are taking on this semester then print it and its data type?
+
+# Create a variable T and store current temperature then print it and its data type?
+
+# Create a variable n, s, q and store your name, your favorite song and your favorite quote respectively. then print it?
+
+# Write any python code that you have learned before then describe your code by title of code, name, date, place
+
+# Create a list of your favorite subjects then print it and its data type?
+
+# Create a tuple of days in a week then print it and its data type?
+
+# Create a set of your favorite books then print it and its data type?
+
+# Create a football player and jersey pair of dictionary then print it and its data type?
+
+# Create a string variable name and store your name then copy it to a new variable, after that print both variables and their memory location?
+
+# Create two integer variables a and b accept values from a user then calculate their average?
+
+# Create two integer variables avg and b accept from user then calculate their average using compound operators?
+
+# Create two variables my_age and friend_age accept from user then compare them?
+
+# Create two variables a and b then compare them?
+
+
+
+
+
+
