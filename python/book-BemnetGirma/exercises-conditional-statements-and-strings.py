65347fd91b2a7e7ebc9a860b9a1d4bfd3225fc6b
Siddardh Garlapati <siddardhr1@gmail.com>
2025-09-23 06:13:17 -0500

    Added: exercises-conditional-statements-and-strings.py

---
+++ python/book-BemnetGirma/exercises-conditional-statements-and-strings.py
@@ -0,0 +1,44 @@
+# Create a variable "age" and take input from user then check vote eligibility (age > 18)?
+
+# Create a variable "username" and its value is your name then create log in conditional statement based on username?
+
+# Create two variable and "nationality" and "cgpa" and store values is your name and nationality, then create scholarship conditional statement based on nationality and cgpa?
+
+# Create two variable "username" and "password" and store values is your name and password, then create log in conditional statement based on username and password?
+
+# Create a variable "score" and take input from user, then classify them based on their score?
+
+# Create two integer variables "a" and "b" take input from a user, then calculate their average by using eval() function?
+
+# Calculate ln(e), cos(90°), 10 the power of 5, Square root of 625, Ceil and Floor of 6.5
+
+# Create a string variable "name" and store your name then print only vowel letters from your name?
+
+# Create a string variable "quote" and store your favorite quote then print each words from the quote?
+
+# Create four string variables "UPPER", "lower", "Symbols", "digits" and store uppercase letters, lowercase letters, special symbols and digits(0-9) respectively then create a password which contains all of them?
+
+# Create a string variable "quote" and store your favorite quote then find length of the quote?
+
+# Create a string variable "name" and store a string “Yonatan” then remove all vowel letters from the string?
+
+# Create username validation program excluding whitespace?
+
+# Create a string variable "quote" and store your favorite quote then find letter "x" in the quote?
+
+# Create a string variable "quote" and store your favorite quote then find index of letter "a" in the quote?
+
+# Create a string variable "name" and store your name then find no. of vowel letters in your name?
+
+# Create a string variable "email" and store your gmail then convert it into yahoo mail?
+
+# Create a string variable "name" and store your full name then split it?
+
+# Create a list variable "name" and store your first name, your middle name and surname then join it?
+
+# Create a Instagram username validation program using the following rules? Minimum 5 characters Maximum 10 characters Only lowercase letters Underscore and dot is allowed Whitespace, symbol are not allowed
+
+
+
+
+
