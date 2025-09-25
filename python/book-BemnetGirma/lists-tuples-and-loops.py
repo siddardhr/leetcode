5c970c9c54e796258bedba0917d3553bbc816ed6
Siddardh Garlapati <siddardhr1@gmail.com>
2025-09-25 02:56:33 -0500

    Created: lists-tuples-and-loops.py

---
+++ python/book-BemnetGirma/lists-tuples-and-loops.py
@@ -0,0 +1,87 @@
+# Create a list of your email, name, gender and age, then print your name?
+
+
+# Create a list of your email, name, gender and age, then print your name and age?
+
+
+# Create a list of your email, name, gender and age, then update your email and print index of all elements in the list?
+
+
+# Create a list of your email, name, gender and age, then append your password and insert your username to the list at index 1?  
+
+
+# Create a list of your email, name, gender and age, then create another list which contains your birth date, month, year and extend/add it to former list? 
+
+
+# Create a list of your email, name, gender and age, then if the email is not gmail delete it, if the gender is male delete all elements from the list?  
+
+
+# Create a list of name of grade 10 students then sort them from A to Z and Z to A? 
+
+
+# Create a list of name of grade 10 students then classify them as Section 10A and Section 10B students?  
+
+
+# Create a list of name of grade 10 students then add extra student ‘John’ then copy the list?  
+
+
+# Create a nested list of name of grade Section 10A and Section 10B students then add extra student ‘John’ then copy the list? 
+
+
+# Create a list in range 1 to 10 first the computer picks one number and you guess that number then if your guess is correct, you win else you lose? 
+
+
+# Create a list of random five numbers then check whether there is duplicated number or not if it is there delete it, if sum of all numbers is even print maximum of the list else print minimum of the list.   
+
+
+# Create a list of random three numbers 1 to 10, then if 2 is in the list duplicate the list else delete the list?   
+
+
+# Create a tuple of random three numbers 1 to 10, then if 2 is in the tuple sort the tuple else reverse sort then unpack the tuple? 
+
+
+# Print all multiples of 7 from 1 to 100 using while loop?
+
+
+# Print Fibonacci Sequence till 50? A Fibonacci Sequence is a series of numbers where each number is the sum of the two numbers that precede it. 0, 1, 1, 2, 3, 5, 8, …
+
+
+# Find factorial (n!) of given number from user?
+
+
+# Print all perfect square numbers from 0 to 100?
+
+
+# Print all prime numbers from 0 to 100?
+
+
+# Find sum of all prime numbers from 0 to 100?
+
+
+# Print all vowels in a given string from user?
+
+
+# Print all prime numbers from 1 to 100 except numbers divisible by 3?
+
+
+# Print all even digits in a number from user?
+
+
+# Print all vowels in a given string from user?
+
+
+# Create a list using list comprehension that includes only the prime numbers from the original list? 
+
+
+# Draw an Isosceles Triangle /Christmas Tree/ pattern which looks like as follows. Enter the number of rows: 4
+
+
+# Draw Life Span /Hour Glass/ pattern which looks like as follows Enter the number of rows: 4
+
+
+# Build a 6 by 6 multiplication table which looks like as follows Enter the number of rows: 6
+
+
+# Draw an Alphabetical Triangle pattern which looks like as follows. Enter the number of rows: 4
+
+
