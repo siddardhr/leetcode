5f807fceecf0ed5f0ba3d4c3de83154248f51a77
Siddardh Garlapati <siddardhr1@gmail.com>
2025-09-29 08:14:51 -0500

    Updated: exercises-lists-tuples-and-loops.py

--- python/book-BemnetGirma/exercises-lists-tuples-and-loops.py
+++ python/book-BemnetGirma/exercises-lists-tuples-and-loops.py
@@ -26,34 +26,71 @@ print(mydetails)
 
 # Create a list of your email, name, gender and age, then if the email is not gmail delete it, if the gender is male delete all elements from the list?  
 mydetails = ['name','a@yahoo.com','male',40]
-
 if not mydetails[1].endswith('gmail.com'):
   del mydetails[1]
-
 if mydetails[1] == 'male':
   mydetails.clear()
-
 print(mydetails)
 
 # Create a list of name of grade 10 students then sort them from A to Z and Z to A? 
-
+names = ['Student', 'tudent', 'udent', 'dent', 'ent' ,'nt', 'list', 'of', 'studens', 'names']
+print(names.sort())
+print(names.sort(false))
 
 # Create a list of name of grade 10 students then classify them as Section 10A and Section 10B students?  
-
+names = ['Student', 'tudent', 'udent', 'dent', 'ent' ,'nt', 'list', 'of', 'studens', 'names']
+section10A = names[0:5]
+section10B = names[5:]
+print(section10A, section10B)
 
 # Create a list of name of grade 10 students then add extra student ‘John’ then copy the list?  
-
+names = ['Student', 'tudent', 'udent', 'dent', 'ent' ,'nt', 'list', 'of', 'studens', 'names']
+names.extend(['John'])
+newnames = names.copy()
+print(names)
+print(newnames)
 
 # Create a nested list of name of grade Section 10A and Section 10B students then add extra student ‘John’ then copy the list? 
-
+import copy
+names = [['Student', 'tudent', 'udent', 'dent', 'ent' ],['nt', 'list', 'of', 'studens', 'names']]
+names.extend(['John'])
+newnames = copy.deepcopy(names)
+print(newnames)
 
 # Create a list in range 1 to 10 first the computer picks one number and you guess that number then if your guess is correct, you win else you lose? 
+import random
 
+randomnumber = random.randint(1, 10)
+usernumber = int(input("Please enter your choice: "))
+if randomnumber == usernumber:
+    print("You guessed it right. You won.")
+else:
+    print(f"You lost. The actual number was {randomnumber} Try again")
 
 # Create a list of random five numbers then check whether there is duplicated number or not if it is there delete it, if sum of all numbers is even print maximum of the list else print minimum of the list.   
+import random
 
+numbers = [random.randint(1, 10) for _ in range(5)]
+numbersset = set(numbers)
+totalvalue = sum(numbersset)
+print(numbersset)
+
+if totalvalue % 2 == 0:
+    print(max(numbersset))
+else:
+    print(min(numbersset))
 
 # Create a list of random three numbers 1 to 10, then if 2 is in the list duplicate the list else delete the list?   
+import random 
+
+onetotenlist = [random.randint(1,10) for _ in range(5)]
+duplicatelist = []  
+if 2 in onetotenlist:
+    duplicatelist = onetotenlist.copy()
+if duplicatelist:
+    print("Duplicated list:", duplicatelist)
+else:
+    print("2 was not found in the list.")
 
 
 # Create a tuple of random three numbers 1 to 10, then if 2 is in the tuple sort the tuple else reverse sort then unpack the tuple? 
