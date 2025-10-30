745261b8a199080661de241f55239a2642fabfec
Siddardh Garlapati <siddardhr1@gmail.com>
2025-10-29 23:56:10 -0500

    Created: sets-and-dictionaries.py

---
+++ python/book-BemnetGirma/sets-and-dictionaries.py
@@ -0,0 +1,40 @@
+#Create set of files ~ ( Img_01.png, Avatar.mp4 ), add new file ‘Track01.mp3’ then Rename ‘Track01.mp3’ to ‘Music01.mp3’, finally Copy all files to new set (Folder)?
+
+
+#Create set of files ~ ( Img_01.png, Pic.jpeg, Avatar.mp4 ), then delete all image files?
+
+
+#Create two sets Folder_1 and Folder_2, then print all files in Folder_1 and Folder_2?
+
+
+#Create two sets Folder_1 and Folder_2, then delete all duplicated files?
+
+
+#Create two sets Folder_1 and Folder_2, then print all non-duplicated files?
+
+
+#Create two sets Folder_1 & Folder_2, then delete duplicated (subset) folder and keep super folder?
+
+
+#Create a frozen set of files / Zip File /?
+
+
+#Create a contact dictionary which contains phone no. and name as key-value pair then insert a contact Abel, +251 9 12546782, finally update its phone no. to +251 9 23468494?
+
+
+#Create a contact dictionary which contains phone no. and name as key-value pair then search a contact Phone no. of ‘Abel’?
+
+
+#Create a contact dictionary which contains phone no and name then set default Name as ‘Unknown’?
+
+
+#Create a contact dictionary which contains phone no. and name then call to random contact?
+
+
+#Create a contact dictionary which contains phone no. and name then print all phone no. and name of contacts?
+
+
+#Create a contact dictionary which contains phone no. and name then delete contact of Abel?
+
+
+#Create a dictionary from a list of favorite football players, with player names as keys and character counts as values using dictionary comprehension?
